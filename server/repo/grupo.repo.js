0534fd493ee426c6f7baa037e66e8235f99c4bb9
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-03-09 16:17:07 -0600

    Arreglar

--- server/repo/grupo.repo.js
+++ server/repo/grupo.repo.js
@@ -1,55 +1,55 @@
-import { prisma } from "../utils/db.utils.js"
+import { prisma } from "../utils/db.utils.js";
 
 export const crearGrupo = ({ nombre, turno, id }) => {
-    return prisma.grupo.create({
-        data: {
-            nombre_grupo: nombre,
-            turno: turno,
-            id_docente: Number(id),
-        },
-    });
-}
+  return prisma.grupo.create({
+    data: {
+      nombre_grupo: nombre,
+      turno: turno,
+      id_docente: Number(id),
+    },
+  });
+};
 
 export const consultarGrupoPorId = (id) => {
-    return prisma.grupo.findUnique({
-        where: {
-            id_grupo: Number(id),
-        },
-    });
-}
+  return prisma.grupo.findUnique({
+    where: {
+      id_grupo: Number(id),
+    },
+  });
+};
 
 export const consultarGrupoPorNombre = (nombre) => {
-    return prisma.grupo.findUnique({
-        where: {
-            nombre: nombre,
-        },
-    })
-}
+  return prisma.grupo.findUnique({
+    where: {
+      nombre: nombre,
+    },
+  });
+};
 
 export const consultarGrupos = () => {
-    return prisma.grupo.findMany({
-        omit: {
-            id_docente: true,
-        }
-    })
-}
+  return prisma.grupo.findMany({
+    omit: {
+      id_docente: true,
+    },
+  });
+};
 
 export const editarGrupo = ({ id, nombre, turno }) => {
-    return prisma.grupo.update({
-        where: {
-            id_grupo: Number(id),
-        },
-        data: {
-            nombre_grupo: nombre,
-            turno: turno,
-        },
-    });
-}
+  return prisma.grupo.update({
+    where: {
+      id_grupo: Number(id),
+    },
+    data: {
+      nombre_grupo: nombre,
+      turno: turno,
+    },
+  });
+};
 
 export const eliminarGrupoPorId = (id) => {
-    return prisma.grupo.delete({
-        where: {
-            id_grupo: Number(id),
-        },
-    });
-}
+  return prisma.grupo.delete({
+    where: {
+      id_grupo: Number(id),
+    },
+  });
+};
