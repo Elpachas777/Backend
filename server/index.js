707b34de8223ef206042a7a54212b7b0a2ab75f5
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-03-09 15:57:42 -0600

    Despliegue

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 
-import indexRoutes from "./routes/index.routes.js";
-import docenteRoutes from "./routes/docente.routes.js";
-import alumnoRoutes from "./routes/alumno.routes.js";
-import gruposRoutes from "./routes/grupo.route.js";
-import sesionRoutes from "./routes/sesion.routes.js";
-import authRoutes from "./routes/auth.routes.js";
+import { manejadorErrores } from "./middleware/error.middleware.js";
 import adminRoutes from "./routes/admin.routes.js";
-import {manejadorErrores} from "./middleware/error.middleware.js"
+import alumnoRoutes from "./routes/alumno.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import docenteRoutes from "./routes/docente.routes.js";
+import gruposRoutes from "./routes/grupo.route.js";
+import indexRoutes from "./routes/index.routes.js";
+import sesionRoutes from "./routes/sesion.routes.js";
 
 
 const app = express();
-
+ 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
--- server/utils/db.utils.js
+++ server/utils/db.utils.js
@@ -1,14 +1,13 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { PrismaClient } from '../../generated/prisma/client.ts';
+import { PrismaClient } from "../../generated/prisma/client.ts";
 
 const adapter = new PrismaMariaDb({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 10
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 10,
 });
 
 export const prisma = new PrismaClient({ adapter });
-
