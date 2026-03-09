15a67e59a1d2a44663b9ad6a724a5b3fbb7b157b
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-03-08 20:26:53 -0600

    Console

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -2,13 +2,14 @@ import { registrarNuevoAdministrador } from "../services/admin.service.js";
 
 export async function registrarAdmin(req, res, next) {
   try {
+    console.log(req.body);
     await registrarNuevoAdministrador(req.body);
 
     return res.status(201).json({
       tipo: "success",
-      mensaje: "Administrador registrado con éxito"
+      mensaje: "Administrador registrado con éxito",
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
