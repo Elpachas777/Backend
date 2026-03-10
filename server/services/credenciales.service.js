70077a7c64cd50892795d9ee9bdf44078416a476
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-03-09 21:25:34 -0600

    token

--- server/services/credenciales.service.js
+++ server/services/credenciales.service.js
@@ -1,15 +1,19 @@
 import "dotenv/config";
-import { verificarTokenAuth } from "../auth/auth.js"
+import { verificarTokenAuth } from "../auth/auth.js";
 import { ApiError } from "../utils/errores.utils.js";
 
 export const verificarToken = (token) => {
-    try {
-        if (!token) {
-            throw new ApiError("No se encontró el token en las cookies", 401, "Token inexistente")
-        }
-        const tokenDatos = verificarTokenAuth(token);
-        return tokenDatos.rol;
-    } catch (error) {
-        controlErrores(error)
+  try {
+    if (!token) {
+      throw new ApiError(
+        "No se encontró el token en las cookies",
+        401,
+        "Token inexistente",
+      );
     }
-}
+    const tokenDatos = verificarTokenAuth(token);
+    return tokenDatos.rol;
+  } catch (error) {
+    throw new ApiError(error.message, 500, "Error al verificar el token");
+  }
+};
