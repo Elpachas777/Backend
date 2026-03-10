82e8a4984a419e68d1540ab991d48f9b2fdf7473
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-03-09 19:32:28 -0600

    Cors

--- server/index.js
+++ server/index.js
@@ -12,12 +12,18 @@ import gruposRoutes from "./routes/grupo.route.js";
 import indexRoutes from "./routes/index.routes.js";
 import sesionRoutes from "./routes/sesion.routes.js";
 
-
 const app = express();
- 
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(indexRoutes);
 app.use(docenteRoutes);
@@ -27,6 +33,6 @@ app.use(sesionRoutes);
 app.use(authRoutes);
 app.use(adminRoutes);
 
-app.use(manejadorErrores)
+app.use(manejadorErrores);
 
 app.listen(process.env.PORT);
