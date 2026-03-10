102e8c43757cc157a61f185ca99c1b55f0840ae4
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-03-09 20:54:51 -0600

    prueba

--- server/utils/db.utils.js
+++ server/utils/db.utils.js
@@ -2,14 +2,20 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
 import { PrismaClient } from "../../generated/prisma/client.ts";
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: Number(process.env.DATABASE_PORT),
-  connectionLimit: 10,
-  ssl: { rejectUnauthorized: false },
-});
+const globalPrisma = globalThis;
 
-export const prisma = new PrismaClient({ adapter });
+if (!globalPrisma.prisma) {
+  const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: Number(process.env.DATABASE_PORT),
+    connectionLimit: 10,
+    ssl: { rejectUnauthorized: false },
+  });
+
+  globalPrisma.prisma = new PrismaClient({ adapter });
+}
+
+export const prisma = globalPrisma.prisma;
