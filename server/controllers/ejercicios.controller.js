c663efa88307011a35ad8fc5e3bd99bd61b46e4b
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-04-25 17:43:03 -0600

    Json

--- server/controllers/ejercicios.controller.js
+++ server/controllers/ejercicios.controller.js
@@ -2,11 +2,9 @@ import { guardarEjercicio } from "../services/ejercicios.service.js";
 
 export const registrarEjercicio = (req, res, next) => {
   try {
-    console.log("hOLA");
-    console.log(req.body);
     const { data } = req.body;
-    guardarEjercicio(data);
-    return "Hola";
+    guardarEjercicio(JSON.stringify(data));
+    return "Guardado";
   } catch (error) {
     next(error);
   }
