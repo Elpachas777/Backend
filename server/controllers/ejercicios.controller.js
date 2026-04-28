f3fdd77d8447adee49af306cda778c7c77920b6c
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-04-28 09:36:20 -0600

    a

--- server/controllers/ejercicios.controller.js
+++ server/controllers/ejercicios.controller.js
@@ -3,7 +3,7 @@ import { guardarEjercicio } from "../services/ejercicios.service.js";
 export const registrarEjercicio = async (req, res, next) => {
   try {
     const data = req.body;
-    const docente = req.cookies?.access_token.id || "";
+    const docente = req.cookies?.access_token || "";
     console.log(docente)
     console.log(req.cookies)
     await guardarEjercicio(JSON.stringify(data), docente);
