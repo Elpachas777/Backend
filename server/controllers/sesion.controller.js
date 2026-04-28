34d4661aad24f8fefd35e051fdebdadc0b02c600
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-04-27 18:06:31 -0600

    tokens

--- server/controllers/sesion.controller.js
+++ server/controllers/sesion.controller.js
@@ -1,15 +1,21 @@
-import { correoRecuperarContraseña, login, modificarContraseña } from "../services/sesion.service.js";
 import { generarToken, objetoSesion } from "../auth/auth.js";
 import { enviarCorreoContraseña } from "../services/correo.service.js";
+import {
+  correoRecuperarContraseña,
+  login,
+  modificarContraseña,
+} from "../services/sesion.service.js";
 
 export async function iniciarSesion(req, res, next) {
   try {
     const sesion = await login(req.body);
+    console.log(sesion);
     const token = generarToken(sesion, "1h");
+    console.log(token);
 
     return res.cookie("access_token", token, objetoSesion()).json(token);
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
@@ -24,20 +30,20 @@ export async function correoContraseña(req, res, next) {
       mensaje: "Siga las instrucciones que se acaban de enviar a su correo.",
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
 export async function recuperarContraseña(req, res, next) {
   try {
-    await modificarContraseña(req.body)
+    await modificarContraseña(req.body);
 
     res.status(200).json({
       tipo: "success",
-      mensaje: "Contraseña actualizada con exito."
+      mensaje: "Contraseña actualizada con exito.",
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
@@ -45,6 +51,6 @@ export async function cerrarSesion(req, res, next) {
   try {
     return res.clearCookie("access_token", objetoSesion()).send("ok");
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
