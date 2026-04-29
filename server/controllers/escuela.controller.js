57796afed5275f655c02f6edd4a616ac2ae26d08
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-04-28 21:26:00 -0600

    import

--- server/controllers/escuela.controller.js
+++ server/controllers/escuela.controller.js
@@ -1,10 +1,10 @@
 import { verInfoEscuelas } from "../services/escuela.service.js";
 
 export const verEscuelas = async (req, res, next) => {
-    try {
-        const escuelas = await verInfoEscuelas()
-        return res.status(200).json(escuelas)
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const escuelas = await verInfoEscuelas();
+    return res.status(200).json(escuelas);
+  } catch (error) {
+    next(error);
+  }
+};
