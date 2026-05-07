f8d004dd28a99d0a3c641f255ccccff73d3228d0
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-05-06 23:50:05 -0600

    si

--- server/controllers/credenciales.controller.js
+++ server/controllers/credenciales.controller.js
@@ -2,14 +2,14 @@ import { verificarToken } from "../services/credenciales.service.js";
 
 export async function obtenerCredencial(req, res, next) {
   try {
-    const token = req.cookies?.access_token || "";
+    const token = req.cookies.access_token;
     const verificado = verificarToken(token);
 
     const data = {
-      nombre : verificado.nombre,
-      rol : verificado.rol
-    }
-    
+      nombre: verificado.nombre,
+      rol: verificado.rol,
+    };
+
     return res.status(200).json(data);
   } catch (error) {
     next(error);
