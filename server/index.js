90d55d896ad20f67ba176e0c7d91314f63d07252
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-05-10 17:24:36 -0600

    si

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://frontend-one-beta-92.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
