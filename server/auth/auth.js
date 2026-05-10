db9fb972b75575b20322c82c098b457cd73ab3d2
Elpachas777 <josedavidbaranquiroz2@gmail.com>
2026-05-10 15:02:15 -0600

    cositas

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -21,7 +21,7 @@ export function generarToken(objeto, duracion) {
 export function objetoSesion() {
   return {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "none",
     maxAge: 1000 * 60 * 60,
   };
