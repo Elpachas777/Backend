998b79b35b0a32e4d9d64ff863b1336701f34bec
Camacho1509 <camacho1509mendez@gmail.com>
2026-05-12 02:27:25 -0600

    fix: comentar ruta estadisticasAsignacion sin handler

--- server/routes/ejercicio.routes.js
+++ server/routes/ejercicio.routes.js
@@ -13,7 +13,7 @@ const router = Router();
 router.post("/crearEjercicio", registrarEjercicio);
 router.get("/obtenerTipos", listarTipos);
 router.get("/obtenerEjercicios", listar);
-router.get("/estadisticasAsignacion/:id", estadisticasAsignacion);
+
 router.put("/editarEjercicio/:id", actualizar);
 router.delete("/eliminarEjercicio/:id", eliminar);
 router.put("/asignar/:id", asignar)
