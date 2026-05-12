ca4927af99fb6e747df14864f4ff9a7820c99de7
Camacho1509 <camacho1509mendez@gmail.com>
2026-05-12 03:24:14 -0600

    fix: agregar import estadisticasAsignacion en escuela.routes.js

--- server/routes/escuela.routes.js
+++ server/routes/escuela.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import {
   actualizar,
+  asignar,
+  eliminar,
+  estadisticasAsignacion,
   listar,
   listarTipos,
   registrarEjercicio,
-  eliminar,
-  asignar
 } from "../controllers/ejercicios.controller.js";
 
 const router = Router();
@@ -16,6 +17,6 @@ router.get("/obtenerEjercicios", listar);
 router.get("/estadisticasAsignacion/:id", estadisticasAsignacion);
 router.put("/editarEjercicio/:id", actualizar);
 router.delete("/eliminarEjercicio/:id", eliminar);
-router.put("/asignar/:id", asignar)
+router.put("/asignar/:id", asignar);
 
 export default router;
